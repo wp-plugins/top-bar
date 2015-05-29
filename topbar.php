e8ae0a962696804b236ab343f6786d8663aeb310
spwebguy <spwebguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 05:47:57 +0000

    support link
    git-svn-id: https://plugins.svn.wordpress.org/top-bar/trunk@1169863 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- topbar.php
+++ topbar.php
@@ -1,7 +1,7 @@
 <?php
 /**
 Plugin Name: Top Bar
-Plugin URI: http://wpdarko.com/support/documentation/get-started-top-bar/
+Plugin URI: http://wpdarko.com/top-bar/
 Description: Simply the easiest way to add a topbar to your website. This plugin adds a simple and clean notification bar at the top your website, allowing you to display a nice message to your visitors. Find support and information on the <a href="http://wpdarko.com/top-bar/">plugin's page</a>. This is a free plugin, it is NOT limited and does not contain any ad. Check out the <a href='http://wpdarko.com/items/top-bar-pro/'>PRO version</a> for more great features.
 Version: 1.1
 Author: WP Darko
